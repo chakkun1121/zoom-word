0af6f14edfcdd2d7eae3d89201617e7a648ea32b
chakkun1121 <chakkun1121haruki@gmail.com>
2023-05-25 12:43:54 +0900

    SEO

--- app/page.js
+++ app/page.js
@@ -5,8 +5,33 @@ import Main from "./main";
 export const metadata = {
   title: "文字を拡大 | chakkun1121",
   description:
-    "文字を拡大するだけのページです。漢字などの文字を拡大したいときに使ってください。",
+    "文字を拡大するだけのページです。漢字や怪しいURLなどの文字を拡大したいときに使ってください。",
+  alternates: {
+    canonical: "https://zoom-word.vercel.app/",
+  },
+  openGraph: {
+    title: "文字を拡大 | chakkun1121",
+    description:
+      "文字を拡大するだけのページです。漢字や怪しいURLなどの文字を拡大したいときに使ってください。",
+    url: "https://zoom-word.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://zoom-word.vercel.app/ogp.png",
+        width: 1200,
+        height: 630,
+        alt: "文字を拡大 | chakkun1121",
+      },
+    ],
+  },
+  twitter: {
+    title: "文字を拡大 | chakkun1121",
+    description:
+      "文字を拡大するだけのページです。漢字や怪しいURLなどの文字を拡大したいときに使ってください。",
+    cardType: "summary_large_image",
+  }
 };
+
 export default function App() {
   return (
     <>
